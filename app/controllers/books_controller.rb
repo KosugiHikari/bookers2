e5617cbd11f538d9b6b23361ac72300dd4ddb0d8
EC2 Default User <ec2-user@ip-172-31-38-252.ap-northeast-1.compute.internal>
2023-02-16 07:12:05 +0000

    [Update] レイアウト修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -57,8 +57,8 @@ class BooksController < ApplicationController
   end
 
   def is_matching_login_user
-    user_id = params[:id].to_i
-    unless user_id == current_user.id
+    book = Book.find(params[:id])
+    unless book.user_id == current_user.id
       redirect_to books_path
     end
   end
